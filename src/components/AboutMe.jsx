9310160c8fcc45d7f69ab4a9c60b4f2b7e385fa3
Shelby Hutchinson <shhutc18@gmail.com>
2024-03-25 14:01:52 -0700

    about me section with styling and personal photo

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import myPicture from '../assets/images/Self.jpg'; // Replace with the path to your picture
+
+const AboutMe = () => {
+  const aboutMeStyle = {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '10px',
+  };
+
+  const imageStyle = {
+    width: '29%', // Make the image smaller
+    marginRight: '20px',
+    borderRadius: '10px', // Add rounded corners
+    border: '5px solid lightgrey',
+  };
+
+  const textStyle = {
+    width: '80%', // Adjust as needed
+    fontSize: '25px',
+    fontFamily: "'Work Sans', sans-serif",
+  };
+
+  return (
+    <div style={aboutMeStyle}>
+      <img src={myPicture} alt="Me" style={imageStyle} />
+      <div style={textStyle}>
+        <p>
+          Hi there! Nice to meet you and welcome to my personal portfolio. Whether you are looking to learn more about me personally
+          or just take a look at the projects I have worked on, you have come to the right place. I am a web developer with a passion
+          of creating and designing websites with a purpose. I am always looking for new opportunities to learn and grow in the field of web development.
+        </p>
+        <p>
+          I graduated from the University of Oregon with a Full Stack Web Developement Certificate and have been working on projects ever since.
+        </p>
+        <p>
+          When I am not coding, I enjoy spending time with my family, friends and 3 fur babies, hiking, and traveling. Video games are also a big part of my life, so if you have any recommendations, I would love to hear them!
+        </p>
+        <p>
+            Please feel free to reach out to me if you have any questions or would like to work together. I am always open to new opportunities and connections. In the meantime, feel free to take a look around and enjoy!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default AboutMe;
