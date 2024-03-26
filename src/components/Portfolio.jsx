93a37c4dfeca9d6407893a416fead56e1d5fff00
Shelby Hutchinson <shhutc18@gmail.com>
2024-03-25 20:25:21 -0700

    added starter portfolio component

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Portfolio = () => {
   const portfolioItems = [
-    { title: 'App 1', img: 'path/to/image1.jpg', text: 'Description for image 1', appLink: 'https://app1.com', repoLink: 'https://github.com/user/repo1' },
+    { title: 'Poke-Pedia', img: '../assets/images/Final.png', text: 'Description for image 1', appLink: 'https://shhutc18.github.io/Poke-Pedia/', repoLink: 'https://github.com/shhutc18/Poke-Pedia' },
     { title: 'App 2', img: 'path/to/image2.jpg', text: 'Description for image 2', appLink: 'https://app2.com', repoLink: 'https://github.com/user/repo2' },
     { title: 'App 3', img: 'path/to/image3.jpg', text: 'Description for image 3', appLink: 'https://app3.com', repoLink: 'https://github.com/user/repo3' },
     { title: 'App 4', img: 'path/to/image4.jpg', text: 'Description for image 4', appLink: 'https://app4.com', repoLink: 'https://github.com/user/repo4' },
@@ -17,8 +17,8 @@ const Portfolio = () => {
           <div style={{ width: '50%' }}>
             <img src={item.img} alt={item.title} style={{ width: '100%', objectFit: 'cover' }} />
             <h3>{item.title}</h3>
-            <a href={item.appLink} target="_blank" rel="noopener noreferrer">View App</a>
-            <a href={item.repoLink} target="_blank" rel="noopener noreferrer">View Code</a>
+            <a href={item.appLink} target="_blank" rel="noopener noreferrer">Link to live application</a>
+            <a href={item.repoLink} target="_blank" rel="noopener noreferrer">Link to code repository</a>
           </div>
           <p style={{ width: '50%', padding: '20px' }}>{item.text}</p>
         </div>
